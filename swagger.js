704a4933850f11b2c2e794314f00e0e02680ab17
Travis Nelson <tnelson4@byupathway.edu>
2026-05-09 04:54:30 -0600

    fix: enforce HTTPS scheme for Swagger documentation

--- swagger.js
+++ swagger.js
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import swaggerAutogen from 'swagger-autogen';
 
 const host = process.env.HOST || 'localhost:3000';
-const schemes = host.startsWith('localhost') ? ['http'] : ['https'];
+const schemes = 'https';
 
 const doc = {
     info: {
